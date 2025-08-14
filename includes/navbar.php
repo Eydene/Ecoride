221874da136135aea9c13918cc82436d4a27b2dc
Thibault Le Grives <corsairgh@gmail.com>
2025-08-14 16:02:42 +0200

    Initialisation projet Ecoride

---
+++ includes/navbar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- Section Header    -->
+    <section class="header">
+        <h1>Voyagez logique avec le covoiturage écologique !</h1>
+        <h3>Trouver un covoiturage</h3>
+        <a href="covoiturage.html" class="btn">Covoiturage</a>
+    </section>
+</body>
+
+</html>
